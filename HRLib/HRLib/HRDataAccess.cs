705c56f65f29514d18e52a0bfed74e593c63a80e
victor chin <Victor@objectmage.com>
2016-09-27 14:37:01 -0400

    Add GetFullHRDataSet method

---
+++ HRLib/HRLib/HRDataAccess.cs
@@ -0,0 +1,40 @@
+﻿using HRLib.HRTableAdapters;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace HRLib
+{
+    public class HRDataAccess
+    {
+        private string _connStr;
+        public HRDataAccess(string connStr)
+        {
+            this._connStr = connStr;
+        }
+        public HR GetFullHRDataSet()
+        {
+            HR ds = new HR();
+            OracleConnection conn = new OracleConnection(_connStr);
+            EMPLOYEESTableAdapter eta = new EMPLOYEESTableAdapter() 
+            { Connection = conn };
+            eta.Fill(ds.EMPLOYEES);
+            
+            DEPARTMENTSTableAdapter dta = new DEPARTMENTSTableAdapter() 
+            { Connection = conn };
+            dta.Fill(ds.DEPARTMENTS);
+
+            LOCATIONSTableAdapter lta = new LOCATIONSTableAdapter() 
+            { Connection = conn };
+            lta.Fill(ds.LOCATIONS);
+           
+            eta.Dispose();
+            dta.Dispose();
+            lta.Dispose();
+            return ds;
+
+        }
+    }
+}
