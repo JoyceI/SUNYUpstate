da6936ed5e06fc96c53d4966ea1acbd91c5ff214
victor chin <Victor@objectmage.com>
2016-09-27 13:15:04 -0400

    Upload HRLib

--- AbcConstruction/AbcConstruction/Program.cs
+++ AbcConstruction/AbcConstruction/Program.cs
@@ -17,12 +17,28 @@ namespace AbcConstruction
             Window[] houseWindows = {   new Window(70, 25), 
                                         new Window(90, 25), 
                                         new Window(50, 25) };
+
+            var q = from w in houseWindows.AsQueryable<Window>()
+                    where w.Width >= 70
+                    orderby w
+                    select w;
+            foreach (var item in q.Take(1))
+            {
+                Console.WriteLine(item.Height);
+            }
+
+
+            
             foreach (var item in houseWindows)
             {
                 int z = 100;
                 item.OnBroken += (x, y) => 
                     Console.WriteLine("Are you kidding for parts:{0} {1:c}??!! {2}",x,y,z);
-              // here is some changes
+
+                item.OnBroken += (x, y) =>
+                 Console.WriteLine("Thats cheap parts:{0} {1:c}??!! {2}", x, y, z);
+            
+                // here is some changes
             }
 
             for (int i = 0; i < 200; i++)
