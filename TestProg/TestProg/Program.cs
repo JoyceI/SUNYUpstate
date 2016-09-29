19b262569349a4c373c356917c23232ab4e9b9da
victor chin <Victor@objectmage.com>
2016-09-29 14:11:06 -0400

    Add Test Prog for CommandLine xlsx generation

---
+++ TestProg/TestProg/Program.cs
@@ -0,0 +1,32 @@
+﻿using ExcelUtils;
+using Oracle.ManagedDataAccess.Client;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProg
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {   
+            if(args.Length!=3)
+            {
+                Console.WriteLine("Sample Usage: TestProg.exe \"filename\" \"reportname\" \"query\" ");
+                // TestProg.exe "c:\Lab\demo.xlsx" "My Report" "Select * from hr.employees"
+                return -1;
+            }
+            DRtoExcel writer = new DRtoExcel(args[0]);
+            using (var conn = new
+                OracleConnection("Data Source=xe;user id=hr;password=oracle"))
+            {
+                writer.ReportName = args[1];
+                writer.WriteDR(conn, args[2]);
+            }
+            return 0;
+        }
+    }
+}
