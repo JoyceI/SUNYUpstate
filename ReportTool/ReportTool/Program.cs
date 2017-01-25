ac785af3a569ee55f79090731ad10985fb732b7d
Victor Chin <victor@objectmage.com>
2017-01-25 13:01:51 -0500

    Add ReportTool

---
+++ ReportTool/ReportTool/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReportTool.RS;
+using System.Net;
+using System.IO;
+namespace ReportTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string report = "/SUNY Practice/CustomerList";
+            string format = "PDF";
+            ReportExecutionServiceSoapClient
+                proxy = new ReportExecutionServiceSoapClient();
+
+         
+            string fileOut = @"C:\Lab\test.pdf";
+            WebClient client = new WebClient();
+            string url = string.Format("http://ssrs.objectmage.com/reportserver?{0}&rs:format={1}",
+                report,
+                format);
+
+            client.Credentials = new NetworkCredential("student", "password");
+            byte[] file = client.DownloadData(url);
+            File.WriteAllBytes(fileOut, file);
+
+            
+
+ 
+            
+
+
+
+        }
+    }
+}
